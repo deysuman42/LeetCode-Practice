9a2cf46a3c2e29c3ec004e00446a2c82d6046aca
Suman Dey <deysuman42@gmail.com>
2021-12-27 19:14:57 +0530

    Update merge-intervals.py

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -2,35 +2,17 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
         intervals.sort()
-        out = []
-      
-        if len(intervals) == 2:
-            if intervals[0][1] >= intervals[1][0]:
-                out.append([min(intervals[0][0],intervals[1][0]), max(intervals[0][1], intervals[1][1])])
-                return out
-            else:
-                return intervals
-            
-        elif len(intervals) == 1:
-            return intervals
+        out = [intervals[0]]
         
-        elif len(intervals) > 2:
-            if intervals[0][1] >= intervals[1][0]:
-                out.append([min(intervals[0][0],intervals[1][0]), max(intervals[0][1], intervals[1][1])])
+        for i in range(1, len(intervals)):
+            if out[-1][1] >= intervals[i][0]:
+                # out.append([min(out[-1][0],intervals[i][0]), max(out[-1][1], intervals[i][1])])
+                out[-1][1] = max(out[-1][1], intervals[i][1])
+                # out.pop(-2)
             else:
-                print(intervals[:2][0])
-                out = intervals[:2]
-            print(out)
-            for i in range(2, len(intervals)):
-                print(out[-1][1])
-                if out[-1][1] >= intervals[i][0]:
-                    out.append([min(out[-1][0],intervals[i][0]), max(out[-1][1], intervals[i][1])])
-                    print(out)
-                    out.pop(-2)
-                    # out.append([out[-1][0], intervals[i][1]])
-                else:
-                    out.append(intervals[i])
-            
+                out.append(intervals[i])
+      
+
         return out
                 
             
