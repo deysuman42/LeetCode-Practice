704b7a4660558fc818ba9354898c062f484e169f
Suman Dey <deysuman42@gmail.com>
2021-12-27 18:15:17 +0530

    Time: 80 ms (33.91%), Space: 17.4 MB (84.15%) - LeetHub

---
+++ meeting-rooms/meeting-rooms.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        
+        intervals.sort()
+        
+        for i in range(0, len(intervals) - 1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        return True
+        # print(list(intervals))
+        
