33929bd3e95309b6870a899c796d9fbdb7b57740
Suman Dey <deysuman42@gmail.com>
2022-02-09 21:58:57 +0530

    Time: 126 ms (37.60%), Space: 15.6 MB (84.25%) - LeetHub

---
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        h = collections.Counter(nums)
+        res = 0
+        for i in h:
+            if (k > 0 and (i + k) in h) or (k == 0 and h[i] > 1):
+                res += 1
+        return res
+                
+    
