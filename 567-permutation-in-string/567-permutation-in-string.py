99462904e7aba20ca985791f93d1706b083cfb3a
Suman Dey <deysuman42@gmail.com>
2022-02-11 11:15:20 +0530

    Time: 1793 ms (27.45%), Space: 13.9 MB (93.32%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,13 +3,8 @@ class Solution:
         
         len_s1 = len(s1)
         len_s2 = len(s2)
-        
         h_s1 = Counter(s1)
-        h_s2 = Counter()
-        h_s2[s2[0]] += 1 
-        prev_key = s2[0]
-    
-    
+       
         for i in range(0, len_s2- (len_s1 - 1)):
             
             temp = s2[i:i+len_s1]
@@ -18,13 +13,5 @@ class Solution:
             
         return False
             
-#             if len(h_s1.keys()) == len(h_s2.keys()):
-                
-#                 if 
-                
-            
-#             if h_s1 == h_s2:
-#                 return True
-#             if len()
-            
+
         
