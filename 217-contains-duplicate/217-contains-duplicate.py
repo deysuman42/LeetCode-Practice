207f1a4bd372da20d78b5e12e766f4958e2d809f
Suman Dey <deysuman42@gmail.com>
2022-06-26 08:10:55 +0530

    Time: 934 ms (6.54%), Space: 26 MB (29.92%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        h = defaultdict(int)
+        
+        for i in nums:
+            if i not in h:
+                h[i] += 1
+            else:
+                return True
+        return False
