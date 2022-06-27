a3802677cba9aaa9f9dc73bb02427fc56503caa7
Suman Dey <deysuman42@gmail.com>
2022-06-27 15:45:00 +0530

    Time: 1029 ms (5.01%), Space: 25.9 MB (93.04%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,21 +1,9 @@
-from collections import defaultdict
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        
-#         h = defaultdict(int)
-        
-#         for i in nums:
-#             if i not in h:
-#                 h[i] += 1
-#             else:
-#                 return True
-#         return False
-
-        hashset = set()
-        
-        for n in nums:
-            if n in hashset:
+        h = collections.Counter(nums)
+        for i in h.values():
+            if i > 1:
                 return True
-            hashset.add(n)
         return False
+      
