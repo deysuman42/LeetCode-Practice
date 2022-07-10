1538184a325c69d3a89007c3e4d0391b60859653
Suman Dey <deysuman42@gmail.com>
2022-07-10 12:03:41 +0530

    Time: 296 ms (8.73%), Space: 15 MB (41.28%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,43 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
+        
+        
+        # numbers.sort()
+        res = []
+
         left = 0
         right = len(numbers) - 1
-        
-        while left < right:
+
+        while (left < right):
             
-            if (numbers[left] + numbers[right]) == target:
-                return [left+1, right+1]
-            elif (numbers[left] + numbers[right]) > target:
-                right -= 1
-            elif (numbers[left] + numbers[right]) < target:
+            two_sum = numbers[left] + numbers[right]
+            if two_sum == target:
+                # res.append(left + 1, right + 1)
+                return [left + 1, right + 1]
                 left += 1
-        return [-1, -1]
+                
+                while (left < right) and (nums[left] == nums[left - 1]):
+                    left += 1
+
+            elif two_sum > target:
+                right -= 1
+            else:
+                left += 1
+        return res
+        
+#         left = 0
+#         right = len(numbers) - 1
+        
+#         while left < right:
+            
+#             if (numbers[left] + numbers[right]) == target:
+#                 return [left+1, right+1]
+#             elif (numbers[left] + numbers[right]) > target:
+#                 right -= 1
+#             elif (numbers[left] + numbers[right]) < target:
+#                 left += 1
+#         return [-1, -1]
         
         
         
