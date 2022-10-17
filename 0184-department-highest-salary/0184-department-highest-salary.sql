f7f059bb7db86761e02c5028033d837b4580dd26
Suman Dey <deysuman42@gmail.com>
2022-10-17 15:09:45 +0530

    Time: 576 ms (88.88%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,24 +1,36 @@
 # Write your MySQL query statement below
 
-select
-  department,
-  employee,
-  salary
-from
-  (
-    Select
+# select
+#   department,
+#   employee,
+#   salary
+# from
+#   (
+#     Select
+#       d.name as 'department',
+#       e.name as 'employee',
+#       e.salary,
+#       dense_rank() over(
+#         partition by e.departmentId
+#         order by
+#           e.salary desc
+#       ) as rnk
+#     from
+#       employee e
+#       inner join department d on e.departmentid = d.id
+#   ) x
+# where
+#   rnk = 1;
+
+Select
       d.name as 'department',
       e.name as 'employee',
-      e.salary,
-      dense_rank() over(
-        partition by e.departmentId
-        order by
-          e.salary desc
-      ) as rnk
-    from
-      employee e
-      inner join department d on e.departmentid = d.id
-  ) x
-where
-  rnk = 1;
-
+      e.salary
+      
+      from employee e join department d
+      ON e.departmentId = d.id
+      
+      where (e.departmentId, e.salary) in 
+      
+      (select departmentId, max(salary) from employee
+      group by departmentid)
