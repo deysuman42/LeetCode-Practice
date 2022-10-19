d199511c587935b6ba6e5aca28284dc969a352f3
Suman Dey <deysuman42@gmail.com>
2022-10-19 11:02:32 +0530

    Time: 751 ms (52.32%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,21 +1,25 @@
 # Write your MySQL query statement below
 
+# Solution 1 - rank()
+# select
+#   player_id,
+#   first_login
+# from
+#   (
+#     select
+#       player_id,
+#       event_date as first_login,
+#       rank() over (
+#         partition by player_id
+#         order by
+#           event_date
+#       ) as rnk
+#     from
+#       activity
+#   ) x
+# where
+#   rnk = 1
 
-select
-  player_id,
-  first_login
-from
-  (
-    select
-      player_id,
-      event_date as first_login,
-      rank() over (
-        partition by player_id
-        order by
-          event_date
-      ) as rnk
-    from
-      activity
-  ) x
-where
-  rnk = 1
+
+# Solution 2 - first_value()
+select  distinct player_id, first_value(event_date) over(partition by player_id order by event_date) first_login from Activity;
