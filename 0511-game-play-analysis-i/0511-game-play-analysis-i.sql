6178597631ebb080f29d0b87013f5432d0412315
Suman Dey <deysuman42@gmail.com>
2022-10-19 10:54:18 +0530

    Time: 672 ms (76.31%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,6 +1,21 @@
 # Write your MySQL query statement below
 
 
-select player_id, first_login from (
-select player_id, event_date as first_login, rank() over (partition by player_id order by event_date)as rnk from activity) x
-    where rnk = 1
+select
+  player_id,
+  first_login
+from
+  (
+    select
+      player_id,
+      event_date as first_login,
+      rank() over (
+        partition by player_id
+        order by
+          event_date
+      ) as rnk
+    from
+      activity
+  ) x
+where
+  rnk = 1
