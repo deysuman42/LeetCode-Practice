533dd54dd56997891830a88e86e72da1eb60e062
Suman Dey <deysuman42@gmail.com>
2022-10-19 11:34:34 +0530

    Time: 1112 ms (47.52%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,8 +1,14 @@
 # Write your MySQL query statement below
 
 
+# solution 1 - COALESCE
+# select e.name, b.bonus from 
+# employee e left join bonus b
+# on e.empid = b.empid
+# where COALESCE(b.bonus,0) < 1000;
+
 
 select e.name, b.bonus from 
 employee e left join bonus b
 on e.empid = b.empid
-where COALESCE(b.bonus,0) < 1000;
+where b.bonus < 1000 OR b.bonus is NULL;
