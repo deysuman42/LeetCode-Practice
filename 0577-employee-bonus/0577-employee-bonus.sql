ad81905bde9e1105284002ba8ac2572c4d201e62
Suman Dey <deysuman42@gmail.com>
2022-10-19 11:39:22 +0530

    Time: 1077 ms (51.89%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -7,8 +7,19 @@
 # on e.empid = b.empid
 # where COALESCE(b.bonus,0) < 1000;
 
+# Solution 2 - IS NULL condition
+# select e.name, b.bonus from 
+# employee e left join bonus b
+# on e.empid = b.empid
+# where b.bonus < 1000 OR b.bonus is NULL;
 
+
+# Solution 3 - IFNULL
 select e.name, b.bonus from 
 employee e left join bonus b
 on e.empid = b.empid
-where b.bonus < 1000 OR b.bonus is NULL;
+where IFNULL(b.bonus,0) < 1000;
+
+
+
+
