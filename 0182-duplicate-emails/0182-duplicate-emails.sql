f9112a21f978710f3327f111f9afd15522f5f9cd
Suman Dey <deysuman42@gmail.com>
2022-10-22 12:36:12 +0530

    Time: 526 ms (36.24%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
 
-
+# Solution 1 - Temporary table
 # select email from (select email, count(*) as c from person group by email) a 
 # where c > 1
 
-Select email from person group by email
-having count(id) > 1;
+# Solution 2 - Having
+# Select email from person group by email
+# having count(id) > 1;
+
+# Solution 3 - Self join
+select distinct a.email from person a join person b
+on a.email = b.email and a.id <> b.id;
