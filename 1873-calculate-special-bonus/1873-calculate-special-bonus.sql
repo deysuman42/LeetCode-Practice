1e90a035b6cefa70ec50e2b16f8e9687cd9bc843
Suman Dey <deysuman42@gmail.com>
2022-10-22 12:27:09 +0530

    Time: 804 ms (43.69%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+
+# with T1 as (select employee_id, name, salary
+#            from employees)
+          
+# select * from t1;
+# (CASE
+#         WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+#         WHEN MOD(id, 2) != 0 AND counts = id THEN id
+#         ELSE id - 1
+#     END) AS id,
+           
+# select * from t1;
+
+
+select employee_id, 
+(case when (employee_id % 2 = 1) and substr(name, 1, 1) <> 'M' then salary
+ else 0
+end) as bonus from employees order by employee_id;
