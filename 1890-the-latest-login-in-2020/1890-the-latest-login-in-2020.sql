e93b3ca27308de7e7cc7221eb15ce61e9a4f90a1
Suman Dey <deysuman42@gmail.com>
2022-10-23 10:53:57 +0530

    Time: 632 ms (88.80%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
 
+# Solution 1
+# select user_id, time_stamp as last_stamp from logins
+# where (user_id, time_stamp) in (select user_id, max(time_stamp) from logins 
+#           where EXTRACT(YEAR FROM time_stamp) = 2020   group by user_id)
 
-select user_id, time_stamp as last_stamp from logins
-where (user_id, time_stamp) in (select user_id, max(time_stamp) from logins 
-          where EXTRACT(YEAR FROM time_stamp) = 2020   group by user_id                )
+# Solution 2
+
+
+select user_id, time_stamp as last_stamp from (
+select user_id, time_stamp, row_number() over(partition by user_id order by time_stamp desc) as rnk
+from logins where EXTRACT(YEAR FROM time_stamp) = 2020) x where rnk = 1;
 
