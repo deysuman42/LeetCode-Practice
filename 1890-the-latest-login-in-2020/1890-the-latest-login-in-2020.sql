e1109e563b775d84dd124f71a3d604b5d4950c9f
Suman Dey <deysuman42@gmail.com>
2022-10-23 10:55:34 +0530

    Time: 814 ms (48.92%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -5,10 +5,11 @@
 # where (user_id, time_stamp) in (select user_id, max(time_stamp) from logins 
 #           where EXTRACT(YEAR FROM time_stamp) = 2020   group by user_id)
 
-# Solution 2
+# Solution 2 - Row number
+# select user_id, time_stamp as last_stamp from (
+# select user_id, time_stamp, row_number() over(partition by user_id order by time_stamp desc) as rnk
+# from logins where EXTRACT(YEAR FROM time_stamp) = 2020) x where rnk = 1;
 
-
-select user_id, time_stamp as last_stamp from (
-select user_id, time_stamp, row_number() over(partition by user_id order by time_stamp desc) as rnk
-from logins where EXTRACT(YEAR FROM time_stamp) = 2020) x where rnk = 1;
+select user_id, max(time_stamp) as last_stamp from logins 
+          where EXTRACT(YEAR FROM time_stamp) = 2020   group by user_id
 
