377210b5e031fb66d6003308dc5fb3ff363eae8d
Suman Dey <deysuman42@gmail.com>
2022-10-29 19:14:40 +0530

    Time: 1332 ms (27.43%), Space: 0B (100.00%) - LeetHub

--- 0580-count-student-number-in-departments/0580-count-student-number-in-departments.sql
+++ 0580-count-student-number-in-departments/0580-count-student-number-in-departments.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
-select d.dept_name, coalesce(count(s.student_id), 0) AS student_number
+select d.dept_name, count(s.student_id) AS student_number
 from department d left join student s on d.dept_id = s.dept_id group by d.dept_name
 order by student_number desc, d.dept_name
