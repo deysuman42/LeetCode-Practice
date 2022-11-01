451c20d2801edfd3f1af83086b9b2102b838c24e
Suman Dey <deysuman42@gmail.com>
2022-11-01 21:35:51 +0530

    Time: 1076 ms (28.67%), Space: 0B (100.00%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+select SUBSTRING(trans_date, 1, 7) as month, country, count(id) as trans_count, sum(if(state = 'approved', 1, 0)) as approved_count, sum(amount) as trans_total_amount, sum(if(state = 'approved', amount, 0)) as approved_total_amount from Transactions group by substring(trans_date,1,7), country
+
